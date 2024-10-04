e928869c14cacedc53148697df4ab2c338e1e90c
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-03 23:44:57 -0400

    creating frontend routes to database

---
+++ frontend/src/components/blogService.js
@@ -0,0 +1,9 @@
+const API_URL = 'http://localhost:5000/api/blogs';
+
+export const fetchBlogs = async () => {}
+
+export const addBlog = async () => {}
+
+export const  updateBlog = async () => {}
+
+export const deleteBlog = async () => {}
