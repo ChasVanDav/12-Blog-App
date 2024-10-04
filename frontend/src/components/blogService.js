d661932eecc61b0e6bca642721c751823a80a29a
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-04 07:35:16 -0400

    adding front end post and put requests

--- frontend/src/components/blogService.js
+++ frontend/src/components/blogService.js
@@ -1,5 +1,7 @@
 const API_URL = 'http://localhost:5000/api/blogs';
 
+//------CRUD OPERATIONS-----//
+//GET
 export const fetchBlogs = async () => {
     const response = await fetch(API_URL);
     if (!response.ok) {
@@ -8,6 +10,7 @@ export const fetchBlogs = async () => {
     return response.json();
 };
 
+//POST
 export const addBlog = async (blog) => {
     const response = await fetch(API_URL, {
         method: 'POST',
@@ -22,6 +25,20 @@ export const addBlog = async (blog) => {
     return response.json();
 };
 
-export const  updateBlog = async () => {};
+//PUT
+export const  updateBlog = async (id, blog) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(blog),
+    });
+    if (!response.ok) {
+        throw new Error('Failed to update blog');
+    }
+    return response.json();
+};
 
+//DELETE
 export const deleteBlog = async () => {};
