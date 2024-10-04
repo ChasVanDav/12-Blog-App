225a39ce7c3938cd8dc23f65f24eb28dd0cfba3f
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-03 22:34:01 -0400

    get and put routes

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,30 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({message: "hello from Vanessa's server"});
 });
+//GET route to database querying all blog information
+app.get('/api/blogs', async (req,res) => {
+    try {
+        const allBlogs = await pool.query('SELECT * FROM blogs');
+        res.json(allBlogs.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.satatys(500).send("Error on Vanessa's server");
+    }
+});
+//PUT route to db querying blog by id
+app.get('/api/blogs:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const blog = await pool.query('SELECT * FROM blogs WHERE id = $1', [id]);
+        if (blog.rows.length === 0) {
+            return res.status(404).json({msg: 'Blog not found'})
+        }
+        res.json(blog.rows[0]);
+    }   catch (err) {
+        console.error(err.message);
+        res.status(500).send("Error on Vanessa's server")
+    }
+})
 
 //-----start the server----//
 const PORT = process.env.PORT
